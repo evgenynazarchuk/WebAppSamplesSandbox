6976f35346f8abc96074f48536ba16e776a26221
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-14 21:41:35 +0300

    add grpc

--- AppRunSample/Startup.cs
+++ AppRunSample/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
---
+++ gRpcServiceSample/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc;
+using Grpc.Core;
+
+namespace gRpcServiceSample.Services
+{
+    public class ProductService : Product.ProductBase
+    {
+        public override Task<ProductFull> GetProduct(ProductIdentity request, ServerCallContext context)
+        {
+            return Task.FromResult(new ProductFull
+            {
+                Id = 1,
+                Name = "Product name"
+            });
+        }
+    }
+}
