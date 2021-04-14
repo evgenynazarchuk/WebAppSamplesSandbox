f1f8d166ef3cce85f498a7854d22aba6e00c01f8
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-15 01:35:49 +0300

    fix code

---
+++ ProductGrpcClient/GrpcClientTool.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ProductGrpcClient
+{
+    class GrpcClientTool : IDisposable
+    {
+        private Grpc.Net.Client.GrpcChannel _channel;
+        private object _service;
+
+        public GrpcClientTool(Type service, string address)
+        {
+            this._channel = Grpc.Net.Client.GrpcChannel.ForAddress(address);
+            this._service = Activator.CreateInstance(type: service, args: _channel);
+        }
+
+        public TResponse Request<TResponse, TRequest>(string methodName, TRequest requestBody)
+            where TRequest : class, new()
+            where TResponse : class, new()
+        {
+            var method = this._service.GetType().GetMethods().Where(x => x.Name == methodName).ToList();
+            var options = new Grpc.Core.CallOptions();
+
+            DateTime startDateTime = DateTime.Now;
+            var response = method[1].Invoke(this._service, new object[] { requestBody, options });
+            DateTime endDateTime = DateTime.Now;
+
+            Console.WriteLine($"{startDateTime.ToString("O")}\t{endDateTime.ToString("O")}\t{endDateTime.Subtract(startDateTime)}");
+
+            return response as TResponse;
+        }
+
+        public void Request<TResponse, TRequest>(string methodName, TRequest requestBody, out TResponse responseBody)
+            where TRequest : class, new()
+            where TResponse : class, new()
+        {
+            responseBody = this.Request<TResponse, TRequest>(methodName, requestBody);
+        }
+
+        public void Request<TRequest>(string methodName, TRequest requestBody)
+            where TRequest : class, new()
+        {
+            this.Request<object, TRequest>(methodName, requestBody);
+        }
+
+        public void Dispose()
+        {
+            this._channel.Dispose();
+        }
+    }
+}
--- ProductGrpcClient/Program.cs
+++ ProductGrpcClient/Program.cs
@@ -1,22 +1,64 @@
 ﻿using System;
 using System.Threading.Tasks;
+using ProductGrpcService;
 
 namespace ProductGrpcClient
 {
     class Program
     {
-        static async Task Main()
+        static void Main()
         {
-            using var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new ProductGrpcService.ProductService.ProductServiceClient(channel);
+            //using var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new ProductGrpcService.ProductService.ProductServiceClient(channel);
 
             // async
-            var responseGet = await client.GetProductAsync(new ProductGrpcService.ProductIdentity { Id = 1 });
-            Console.WriteLine(responseGet.Name);
+            //var responseGet = await client.GetProductAsync(new ProductGrpcService.ProductIdentity { Id = 1 });
+            //Console.WriteLine(responseGet.Name);
+            //
+            //// not async
+            //var responseCreate = client.CreateProduct(new ProductGrpcService.ProductFull { Id = 1, Name = "Product name" });
+            //Console.WriteLine(responseCreate.Id);
 
-            // not async
-            var responseCreate = client.CreateProduct(new ProductGrpcService.ProductFull { Id = 1, Name = "Product name" });
-            Console.WriteLine(responseCreate.Id);
+            var serviceType = typeof(ProductGrpcService.ProductService.ProductServiceClient);
+            var address = "https://localhost:5001";
+
+            var client = new GrpcClientTool(serviceType, address);
+
+            ProductFull product1 = null;
+            ProductFull product2 = null;
+            for (var i = 0; i < 10; i++)
+            {
+                product1 = client.Request<ProductFull, ProductIdentity>(
+                    ProductServiceMethods.GetProduct
+                    , new ProductIdentity { Id = 1 }
+                );
+
+                client.Request(
+                    ProductServiceMethods.GetProduct
+                    , new ProductIdentity { Id = 1 }
+                    , out product2
+                );
+
+                client.Request(
+                    ProductServiceMethods.GetProduct
+                    , new ProductIdentity { Id = 1 }
+                    , out ProductFull product3
+                );
+
+                client.Request(
+                    ProductServiceMethods.GetProduct
+                    , new ProductIdentity { Id = 1 }
+                );
+            }
+
+            Console.WriteLine(product1);
+            Console.WriteLine(product2);
+            //Console.WriteLine(product3);
+        }
+
+        public static class ProductServiceMethods
+        {
+            public const string GetProduct = "GetProduct";
         }
     }
 }
