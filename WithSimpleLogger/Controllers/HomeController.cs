fff1121cebc9fa7cc7e34b981872c9834f9d75b9
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-05 20:12:52 +0300

    add simple logger

---
+++ WithSimpleLogger/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WithSimpleLogger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+           // appsettings.json
+           // "WithSimpleLogger.Controllers.HomeController": "Trace"
+
+           // Trace = 0 (trace, debug, information, crtical, error, warning)
+           // Debug = 1 (debug, information, crtical, error, warning)
+           // Information = 2 (information, crtical, error, warning)
+           // Warning = 3 (crtical, error, warning)
+           // Error = 4 (critical, error)
+           // Critical = 5 (only critical)
+           // None = 6 (no loggin)
+            
+
+            _logger.LogCritical("Critical message");
+            _logger.LogDebug("Debug message");
+            _logger.LogError("Error message");
+            _logger.LogInformation("Information message");
+            _logger.LogTrace("Trace message");
+            _logger.LogWarning("Warning message");
+
+            return Ok("Completed");
+        }
+    }
+}
