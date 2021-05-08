98f3fae6d5f157b6968d9956ce188efc51a3c6cb
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-08 20:37:58 +0300

    add response cache

---
+++ AppWithResponseCache/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWithResponseCache.Controllers
+{
+    [Route("{controller=Home}/{action=Index}")]
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        [ResponseCache(Duration = 10000, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(CacheProfileName = "No Cache")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(CacheProfileName = "Public Cache")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(CacheProfileName = "Private Cache")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppWithResponseCache/Startup.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
+namespace AppWithResponseCache
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // for kestrel
+            services.AddResponseCompression(configureOptions =>
+            {
+                configureOptions.EnableForHttps = true;
+                configureOptions.Providers.Clear();
+                configureOptions.Providers.Add(new BrotliCompressionProvider(new BrotliCompressionProviderOptions()
+                {
+                    Level = CompressionLevel.Optimal
+                }));
+                configureOptions.Providers.Add(new GzipCompressionProvider(new GzipCompressionProviderOptions()
+                {
+                    Level = CompressionLevel.Optimal
+                }));
+            });
+
+            // for kestrel
+            //services.Configure<BrotliCompressionProviderOptions>(options =>
+            //{
+            //    options.Level = CompressionLevel.Optimal;
+            //});
+            //
+            //services.Configure<GzipCompressionProviderOptions>(options =>
+            //{
+            //    options.Level = CompressionLevel.Optimal;
+            //});
+
+            services.AddControllersWithViews(configure =>
+            {
+                // control cache (iis, apache, nginx, proxy)
+                configure.CacheProfiles.Add("No Cache", new CacheProfile
+                {
+                    Location = ResponseCacheLocation.None,
+                    NoStore = true
+                });
+
+                configure.CacheProfiles.Add("Public Cache", new CacheProfile
+                {
+                    Duration = 300,
+                    Location = ResponseCacheLocation.Any,
+                    NoStore = false
+                });
+
+                configure.CacheProfiles.Add("Client Cache", new CacheProfile
+                {
+                    Duration = 300,
+                    Location = ResponseCacheLocation.Client,
+                    NoStore = false
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseResponseCompression();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
