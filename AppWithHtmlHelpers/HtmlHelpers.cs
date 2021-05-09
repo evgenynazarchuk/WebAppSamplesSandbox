3d9757c3f8505d669db2fcc85cdf4988480e4092
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-09 14:31:26 +0300

    add html helpers code sample

---
+++ AppWithHtmlHelpers/HtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AppWithHtmlHelpers
+{
+    public static class HtmlHelpers
+    {
+        public static HtmlString PrintList(this IHtmlHelper page, IReadOnlyCollection<string> list)
+        {
+            string html;
+            html = "<div>";
+            foreach (var item in list)
+            {
+                html = $"{html}{item}<br/>";
+            }
+            html = $"{html}</div>";
+            return new HtmlString(html);
+        }
+    }
+}
--- AppWithHtmlHelpers/Program.cs
+++ AppWithHtmlHelpers/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace AppWithViewComponent
+namespace AppWithHtmlHelpers
 {
     public class Program
     {
